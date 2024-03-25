b3f2c13dacd63f6c1c519f68df6a802321c561b4
Matteo Pearce <matteopearce@googlemail.com>
2024-03-25 15:07:51 +0000

    update source field maker to accept array

--- ExtractReservoirOutput.py
+++ ExtractReservoirOutput.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 def ExtractReservoirOutput(file_path:str):
 
@@ -8,9 +8,19 @@ def ExtractReservoirOutput(file_path:str):
         file_path = file_path + "/reservoir_output.txt"
 
     with open(file_path, "r") as file:
+        numpy_array = np.empty((1,len(file.readline())),dtype=float)
         data = file.readlines()
         file.close()
 
-    print((data))
+    for line in data:
+
+        dummy_array = np.asarray(line.join(""))
+        #print(type(dummy_array))
+        print(dummy_array)
+        input()
+        dummy_array = dummy_array.reshape((1,len(line)))
+        numpy_array = np.concatenate((numpy_array,dummy_array),axis=0)
+
+    print(numpy_array)
 
 ExtractReservoirOutput("/home/matteo/Desktop/VAMPIRE_WORKDIR/reservoir_output.txt")
