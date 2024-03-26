d2a270f35aefb3cfb080e04023c330f125294cce
Matteo Pearce <matteopearce@googlemail.com>
2024-03-26 10:46:20 +0000

    Reservoir Output extractor script

--- ExtractReservoirOutput.py
+++ ExtractReservoirOutput.py
@@ -1,25 +1,29 @@
 import numpy as np
 
-def ExtractReservoirOutput(file_path:str) -> np.ndarray:
+def ExtractReservoirIO(file_path1: str,file_path2: str = None) -> np.ndarray:
 
-    if ".txt" in file_path:
-        pass
-    else:
-        file_path = file_path + "/reservoir_output.txt"
+    paths = [file_path1, file_path2]
+    output_arrays = list()
+    if file_path2 is None:
+        paths.pop(1)
 
-    with open(file_path, "r") as file:
-        numpy_array = np.empty((1,len(file.readline().split(" "))-1),dtype=float)
-        data = file.readlines()
-        file.close()
+    for file_path in paths:
 
-    for line in data:
-        dummy_list = line.split(" ")
-        dummy_list.pop(-1)
-        dummy_array = np.asarray(dummy_list,dtype=float)
-        dummy_array = dummy_array.reshape((1,len(dummy_list)))
-        numpy_array = np.concatenate((numpy_array,dummy_array),axis=0)
+        with open(file_path, "r") as file:
+            numpy_array = np.empty((1,len(file.readline().split(" "))-1),dtype=float)
+            data = file.readlines()
+            file.close()
+
+        for line in data:
+            dummy_list = line.split(" ")
+            dummy_list.pop(-1)
+            dummy_array = np.asarray(dummy_list,dtype=float)
+            dummy_array = dummy_array.reshape((1,len(dummy_list)))
+            numpy_array = np.concatenate((numpy_array,dummy_array),axis=0)
+
+        output_arrays.append(numpy_array)
 
     #print(numpy_array.shape)
-    return numpy_array
+    return output_arrays
 
-ExtractReservoirOutput("/home/matteo/Desktop/VAMPIRE_WORKDIR/reservoir_output.txt")
+#ExtractReservoirOutput("/home/matteo/Desktop/VAMPIRE_WORKDIR/reservoir_output.txt")
---
+++ test.py
@@ -0,0 +1,5 @@
+from ExtractReservoirOutput import ExtractReservoirOutput as ERO
+import NeuronOutput
+
+array = ERO("/home/matteo/Desktop/VAMPIRE_WORKDIR/reservoir_output.txt")
+model = NeuronOutput.NeuronOutput()
