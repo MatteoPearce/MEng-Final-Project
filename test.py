8bf71f0f85e4f31cd457d38e59900fa7a276d234
Matteo Pearce <matteopearce@googlemail.com>
2024-03-26 10:50:13 +0000

    Reservoir Output extractor script

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-from ExtractReservoirOutput import ExtractReservoirOutput as ERO
+from ExtractReservoirOutput import ExtractReservoirIO as ERO
 import NeuronOutput
+import numpy as np
 
-array = ERO("/home/matteo/Desktop/VAMPIRE_WORKDIR/reservoir_output.txt")
-model = NeuronOutput.NeuronOutput()
+input_array, output_array = ERO("/home/matteo/Desktop/VAMPIRE_WORKDIR/sourcefield.txt","/home/matteo/Desktop/VAMPIRE_WORKDIR/reservoir_output.txt")
+print(input_array.shape)
+print(output_array.shape)
+#model = NeuronOutput.NeuronOutput()
