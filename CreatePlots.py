Added intrinsic damping and input scaling to CreatePlots.py, fixed reference before assignment bug in Regression_Training.py

CreatePlots.py
@@ -162,7 +162,7 @@ def plotHeatmap(save_path: str = None, data: dict = None) -> None:
                 matrix = np.concatenate((matrix,row), axis=1)
 
         valid_fields = dict_to_plot.keys()
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(figsize=(10,20))
         im = ax.imshow(matrix)
 
         # Show all ticks and label them with the respective list entries
@@ -232,5 +232,6 @@ def plotScatter(save_path: str = None, data: dict = None) -> None:
 
 param_names = ["material:file", "dimensions:system-size-x", "dimensions:system-size-y",
                "dimensions:system-size-z", "cells:macro-cell-size", "sim:applied-field-strength",
-               "sim:applied-field-unit-vector", "sim:temperature"]
+               "sim:applied-field-unit-vector", "sim:temperature","intrinsic magnetic damping",
+                "field intensity input scaling"]
 createPlotData("/home/matteo/Desktop/VAMPIRE_TEST_RESULTS",param_names)

Regression_Training.py
@@ -67,6 +67,7 @@ def trainCV(input_array: np.ndarray,
     if split is not None:
 
         best_result = np.inf
+        updated = False
         if split <= 0.1:
             split = 0.9
 
@@ -91,12 +92,12 @@ def trainCV(input_array: np.ndarray,
 
             if NRMSE is not None:
                 if NRMSE < best_result:
+                    updated = True
                     best_result = NRMSE
                     best_y = y
                     best_y_pred = y_pred
 
-        if best_result is not None:
-
+        if updated:
             return best_result, best_y, best_y_pred
 
         else: