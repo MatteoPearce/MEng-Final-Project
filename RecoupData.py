b1964809e00f4055a16cd2c42e36367e2d1f2785
Matteo Pearce <matteopearce@googlemail.com>
2024-05-08 18:35:29 +0100

    tidied and commented RecoupData.py

--- RecoupData.py
+++ RecoupData.py
@@ -1,4 +1,12 @@
 import os
+
+"""
+EXTRACTS DATA FROM ALL ACCURACY SCORES FILES AND WRITES TO best_iterations.txt, WHICH IS USED BY CreatePlots.py TO
+PLOT NMSE DATA.
+
+threshold set manually to modify depth of data.
+"""
+
 def recoup_data(test_data_path: str, threshold: float = None, training: bool = False):
 
     all_data = str()
@@ -6,33 +14,33 @@ def recoup_data(test_data_path: str, threshold: float = None, training: bool = F
     for dir in os.listdir(test_data_path):
         dirname = os.fsdecode(dir)
 
-        if "FAILED" in dirname:
+        if "FAILED" in dirname: # skip failed runs
             pass
         else:
             for file in os.listdir(os.path.join(test_data_path, dir)):
                 filename = os.fsdecode(file)
                 if filename == "accuracy_scores.txt":
-                    #print(filename)
+
                     with open(os.path.join(test_data_path, dir, file), "r") as f:
                         new_data = f.readlines()
                         f.close()
 
-                    if threshold is not None:
+                    if threshold is not None: # if threshold is specified, check
 
                         for line in new_data:
                             if training:
-                                if "training_NRMSE" in line:
-                                    nrmse = float(line.split(": ")[1])
+                                if "training_NMSE" in line:
+                                    nmse = float(line.split(": ")[1])
                                     break
                             else:
-                                if "NRMSE" in line :
-                                    if "training_NRMSE" in line:
+                                if "NMSE" in line :
+                                    if "training_NMSE" in line:
                                         pass
                                     else:
-                                        nrmse = float(line.split(": ")[1])
+                                        nmse = float(line.split(": ")[1])
                                         break
 
-                        if nrmse < threshold:
+                        if nmse < threshold:
 
                             for line in new_data:
                                 all_data += str(line).replace(": ", " = ")
@@ -40,21 +48,19 @@ def recoup_data(test_data_path: str, threshold: float = None, training: bool = F
                             all_data += "\n"
                             break
 
-                    else:
+                    else: # else grab all
 
                         for line in new_data:
                             all_data += str(line).replace(": ", " = ")
                         all_data += "iteration = " + dirname + "\n"
                         all_data += "\n"
                         break
-
-
-
+    # create file
     with open(os.path.join(test_data_path, "best_iterations.txt"), "w") as f:
         f.write(all_data)
         f.close()
 
 #----------------------------------------------------------------------------------------------------------------------#
-a = "/home/matteo/Desktop/VAMPIRE_TEST_RESULTS"
-b = "/home/matteo/Desktop/VAMPIRE_TEST_RESULTS/NARMA10_GRID49to99_0K/Iteration Files"
-recoup_data( a,threshold=0.3)
+
+dir = "/home/matteo/Desktop/VAMPIRE_TEST_RESULTS"
+recoup_data(dir , threshold=0.3)
