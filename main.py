3562e592cc76783c868fa312a5eaa12db61108be
Matteo Pearce <matteopearce@googlemail.com>
2024-05-18 19:10:54 +0100

    for viking Fe

--- main.py
+++ main.py
@@ -72,9 +72,9 @@ class MaterialEvolution():
     random_input_locs: bool = False
     signal_strength: float = 1.0
     best_result = np.inf # current best, starts at "infinity", 0 being desirable
-    base_workdir_path: str = "/home/matteo/Desktop/VAMPIRE_WORKDIR" # working directory with materials folder and vampire binary "/home/matteo/Desktop/VAMPIRE_WORKDIR"
-    base_materials_path: str = "/home/matteo/Desktop/VAMPIRE_WORKDIR/Materials" # materials folder in working directory "/home/matteo/Desktop/VAMPIRE_WORKDIR/Materials"
-    base_testdata_path: str = "/home/matteo/Desktop/VAMPIRE_TEST_RESULTS" # results depot "/home/matteo/Desktop/VAMPIRE_TEST_RESULTS"
+    base_workdir_path: str = "/users/mp1432/scratch/VAMPIRE_WORKDIR" # working directory with materials folder and vampire binary "/home/matteo/Desktop/VAMPIRE_WORKDIR"
+    base_materials_path: str = "/users/mp1432/scratch/VAMPIRE_WORKDIR/Materials" # materials folder in working directory "/home/matteo/Desktop/VAMPIRE_WORKDIR/Materials"
+    base_testdata_path: str = "/users/mp1432/scratch/VAMPIRE_TEST_RESULTS" # results depot "/home/matteo/Desktop/VAMPIRE_TEST_RESULTS"
     input_file_parameters: dict = { "material:file" : ["Fe.mat"], #"Co.mat"],#,"Fe.mat","Ni.mat"],
                                     "dimensions:system-size-x" : [49],
                                     "dimensions:system-size-y" : [49],
